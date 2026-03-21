633ee4bc1006f5356fb31abc4c93f548a616c228
danilo.sosa@texo.com.py <danilo.sosa@texo.com.py>
2026-03-21 17:00:44 -0300

    fix: exclude static files from middleware

--- middleware.ts
+++ middleware.ts
@@ -65,5 +65,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|api/|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.svg|.*\\.ico|.*\\.webp).*)",
+  ],
 };
